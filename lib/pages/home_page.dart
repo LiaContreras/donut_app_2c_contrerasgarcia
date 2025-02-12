d3a1923bf72c323445b97301435bf05314f5543e
Lilia Contreras <liakiki166gmail.com>
2025-02-12 13:21:57 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,41 @@ class _HomePageState extends State<HomePage> {
         leading: Icon(Icons.menu,
         color:Colors.grey[800]
         ),
-        actions:[
-          Icon(Icons.person)
+        actions: const [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          //Tesxto principal
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+            child: Row(
+              children: [
+                Text("I want to ", style: TextStyle(fontSize: 32 ),), 
+                Text("Eat", 
+                style: TextStyle(
+                  //Tamaño de letra
+                  fontSize: 32,
+            
+                  //Negritas
+                  fontWeight: FontWeight.bold,
+            
+                  //Subrayado 
+                  decoration: TextDecoration.underline) ,)
+              ],
+            ),
+          ),
+          
+
+          //Pestañas(Tapbar)
+
+          //Contenido de pestañas (TabBarView)
+
+          //Carrito(Cart)
         ]
       )
     );
